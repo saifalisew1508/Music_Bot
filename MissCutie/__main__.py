c68a2cc7cf7f3adae7bef5802cb2fbc71dbe124f
SAIF ALI <80207551+saifalisew1508@users.noreply.github.com>
2022-11-29 01:39:43 +0530

    Add files via upload

---
+++ MissCutie/__main__.py
@@ -0,0 +1,81 @@
+#
+# Copyright (C) 2021-2022 by MissCutie@Github, < https://github.com/MissCutie >.
+#
+# This file is part of < https://github.com/MissCutie/MissCutie > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/MissCutie/MissCutie/blob/master/LICENSE >
+#
+# All rights reserved.
+
+import asyncio
+import importlib
+import sys
+
+from pyrogram import idle
+from pytgcalls.exceptions import NoActiveGroupCall
+
+import config
+from config import BANNED_USERS
+from MissCutie import LOGGER, app, userbot
+from MissCutie.core.call import MissCutie
+from MissCutie.plugins import ALL_MODULES
+from MissCutie.utils.database import get_banned_users, get_gbanned
+
+loop = asyncio.get_event_loop()
+
+
+async def init():
+    if (
+        not config.STRING1
+        and not config.STRING2
+        and not config.STRING3
+        and not config.STRING4
+        and not config.STRING5
+    ):
+        LOGGER("MissCutie").error(
+            "No Assistant Clients Vars Defined!.. Exiting Process."
+        )
+        return
+    if (
+        not config.SPOTIFY_CLIENT_ID
+        and not config.SPOTIFY_CLIENT_SECRET
+    ):
+        LOGGER("MissCutie").warning(
+            "No Spotify Vars defined. Your bot won't be able to play spotify queries."
+        )
+    try:
+        users = await get_gbanned()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+        users = await get_banned_users()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+    except:
+        pass
+    await app.start()
+    for all_module in ALL_MODULES:
+        importlib.import_module("MissCutie.plugins" + all_module)
+    LOGGER("MissCutie.plugins").info(
+        "Successfully Imported Modules "
+    )
+    await userbot.start()
+    await MissCutie.start()
+    try:
+        await MissCutie.stream_call(
+            "http://docs.evostream.com/sample_content/assets/sintel1m720p.mp4"
+        )
+    except NoActiveGroupCall:
+        LOGGER("MissCutie").error(
+            "[ERROR] - \n\nPlease turn on your Logger Group's Voice Call. Make sure you never close/end voice call in your log group"
+        )
+        sys.exit()
+    except:
+        pass
+    await MissCutie.decorators()
+    LOGGER("MissCutie").info("MissCutie Music Bot Started Successfully")
+    await idle()
+
+
+if __name__ == "__main__":
+    loop.run_until_complete(init())
+    LOGGER("MissCutie").info("Stopping MissCutie Music Bot! GoodBye")
