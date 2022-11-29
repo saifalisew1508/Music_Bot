c767528ed26a6b2cc27b2c618b980e7876ffa5e4
SAIF ALI <80207551+saifalisew1508@users.noreply.github.com>
2022-11-29 19:34:11 +0530

    Add files via upload

---
+++ MissCutie/plugins/sudo/logger.py
@@ -0,0 +1,38 @@
+#
+# Copyright (C) 2021-2022 by MissCutie@Github, < https://github.com/MissCutie >.
+#
+# This file is part of < https://github.com/MissCutie/MissCutie > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/MissCutie/MissCutie/blob/master/LICENSE >
+#
+# All rights reserved.
+
+from pyrogram import filters
+
+import config
+from strings import get_command
+from MissCutie import app
+from MissCutie.misc import SUDOERS
+from MissCutie.utils.database import add_off, add_on
+from MissCutie.utils.decorators.language import language
+
+# Commands
+LOGGER_COMMAND = get_command("LOGGER_COMMAND")
+
+
+@app.on_message(filters.command(LOGGER_COMMAND) & SUDOERS)
+@language
+async def logger(client, message, _):
+    usage = _["log_1"]
+    if len(message.command) != 2:
+        return await message.reply_text(usage)
+    state = message.text.split(None, 1)[1].strip()
+    state = state.lower()
+    if state == "enable":
+        await add_on(config.LOG)
+        await message.reply_text(_["log_2"])
+    elif state == "disable":
+        await add_off(config.LOG)
+        await message.reply_text(_["log_3"])
+    else:
+        await message.reply_text(usage)
