9b98833d2e70c4d329f7ce6c1303e506b903f420
SAIF ALI <80207551+saifalisew1508@users.noreply.github.com>
2022-12-03 10:28:54 +0530

    Update userbot.py

--- MissCutie/core/userbot.py
+++ MissCutie/core/userbot.py
@@ -48,8 +48,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("StormUpdates")
-                await self.one.join_chat("StormSupportChat")                
+                await self.one.join_chat("MissCutieUpdates")
+                await self.one.join_chat("COLLEGEWALIMASTI")                
             except:
                 pass
             assistants.append(1)
@@ -78,8 +78,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("StormUpdates")
-                await self.two.join_chat("StormSupportChat")
+                await self.two.join_chat("MissCutieUpdates")
+                await self.two.join_chat("COLLEGEWALIMASTI")
             except:
                 pass
             assistants.append(2)
@@ -108,8 +108,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("StormUpdates")
-                await self.three.join_chat("StormSupportChat")
+                await self.three.join_chat("MissCutieUpdates")
+                await self.three.join_chat("COLLEGEWALIMASTI")
             except:
                 pass
             assistants.append(3)
@@ -138,8 +138,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("StormUpdates")
-                await self.four.join_chat("StormSupportChat")
+                await self.four.join_chat("MissCutieUpdates")
+                await self.four.join_chat("COLLEGEWALIMASTI")
             except:
                 pass
             assistants.append(4)
@@ -168,8 +168,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("StormUpdates")
-                await self.five.join_chat("StormSupportChat")
+                await self.five.join_chat("MissCutieUpdates")
+                await self.five.join_chat("COLLEGEWALIMASTI")
             except:
                 pass
             assistants.append(5)
